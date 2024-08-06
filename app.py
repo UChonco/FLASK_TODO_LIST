78e878f2842025f9f414536f2111b21dd38b3c97
UChonco <153912216+UChonco@users.noreply.github.com>
2024-08-06 20:18:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,14 @@ def delete_task(task_id):
     if 0 <= task_id < len(tasks):
         tasks.pop(task_id)
     return redirect(url_for('index'))
+    
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
